9244443531877d59ee80d6e0e794353568bd8871
github username <github email address>
2021-03-15 19:51:30 -0500

    tailwind layout, first fetch request for owner.

--- public/index.js
+++ public/index.js
@@ -1,22 +1,21 @@
-const rootEl = document.getElementById('root')
-
-fetch("http://localhost:3000/rooms")
-    .then((res) => res.json())
-    .then((data) => renderItems(data))
+const endPoint = "http://localhost:3000/rooms"
+const listEl = document.getElementById('list')
+const ownerTitle = document.getElementById('owner')
 
 
-   const renderItems = function(rooms){
-        console.log(rooms)
+document.addEventListener("DOMContentLoaded", () => {
+    getOwner()
+})
 
-        rooms.forEach((room) => {
-            rootEl.innerHTML += `
-            <div>
-                <h2> Home Owner: ${room.home_owner_name}</h2>
-                <h4> bedroom list: ${room.bedroom}<h4>
-                <h4> Living room list: ${room.living_room}<h4>
-                <h4> Kitchen list: ${room.kitchen}<h4>
-                <h4> Dinning room list: ${room.dinning_room}<h4>
-            </div>
-            `;
+function getOwner(){
+    fetch(endPoint)
+    .then(res => res.json())
+    .then(owner =>  {
+            owner.forEach(owner => { 
+                const ownerName = `
+                <h1><strong> ${owner.home_owner_name}'s Home <strong></h1>
+                `
+            document.querySelector('#owner').innerHTML += ownerName
+            })
         })
-    }
+}
