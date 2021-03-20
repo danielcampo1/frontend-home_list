79f6c14b2f51bd2f051e0c240bbf118985682b14
github username <github email address>
2021-03-19 20:35:15 -0500

    got first journal entrie to work

--- public/index.js
+++ public/index.js
@@ -1,20 +1,26 @@
-const endPoint = "http://localhost:3000/rooms"
-
-
+const endpoint ="http://localhost:3000/api/v1/journals" 
 
 document.addEventListener("DOMContentLoaded", () => {
-    getOwner()
+    getJournal()
 })
 
-function getOwner(){
-    fetch(endPoint)
+function getJournal() {
+    fetch(endpoint)
     .then(res => res.json())
-    .then(owner =>  {
-            owner.forEach(owner => { 
-                const ownerName = `
-                <h1><strong> ${owner.home_owner_name}'s Home <strong></h1>
-                `
-            document.querySelector('#owner').innerHTML += ownerName
-            })
+    .then(journal => {
+        journal.data.forEach(journal => {
+            const journalMarkUp = `
+            <div data-id = ${journal.id}>
+                <p> ${journal.attributes.category.name} </p>
+                <h1> ${journal.attributes.title} </h1>
+                <h3> ${journal.attributes.description} </h3>
+                <img src = ${journal.attributes.image_url}>
+                <button data-id = ${journal.id}>Edit </button>
+            </div>
+            <br><br>
+            `
+    document.querySelector("#journal-container").innerHTML += journalMarkUp
+
         })
+    })
 }
